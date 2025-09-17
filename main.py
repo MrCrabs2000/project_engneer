29bf4e57c4934286d2bce97e9225127fc0dce303
MrCrabs2000 <maximth11@yandex.ru>
2025-09-17 15:02:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+from tgbotiha import check_response
+
+
+app = Flask(__name__)
+app.config['TELEGRAM_BOT_TOKEN'] = '8373230853:AAExLeEupdgJyfOZV7o3GtUEiAQZxlWVMr0'
+
+
+@app.route('/')
+def index():
+    return render_template('shit.html')
+
+
+
+@app.route('/login/telegram')
+def login_telegram():
+    data = {
+        'id': request.args.get('id', None),
+        'first_name': request.args.get('first_name', None),
+        'last_name': request.args.get('last_name', None),
+        'username': request.args.get('username', None),
+        'photo_url': request.args.get('photo_url', None),
+        'auth_date': request.args.get('auth_date', None),
+        'hash': request.args.get('hash', None)
+    }
+    if check_response(data):
+        return data
+    else:
+        return 'Authorization failed'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ tgbotiha.py
@@ -0,0 +1,18 @@
+import hmac
+import hashlib
+
+
+def check_response(data):
+    d = data.copy()
+    del d['hash']
+    d_list = []
+    for key in sorted(d.keys()):
+        if d[key] != None:
+            d_list.append(key + '=' + d[key])
+    data_string = bytes('\n'.join(d_list), 'utf-8')
+
+    secret_key = hashlib.sha256(app.config['TELEGRAM_BOT_TOKEN'].encode('utf-8')).digest()
+    hmac_string = hmac.new(secret_key, data_string, hashlib.sha256).hexdigest()
+    if hmac_string == data['hash']:
+        return True
+    return False
